374260d76d317cd5b01ff5c77dd643b93c9e9fe1
tru23 <83947699+tru23@users.noreply.github.com>
2024-07-14 22:25:11 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,23 @@
+const name="trupti"
+const repocount=25
+
+//console.log(name + repocount)
+console.log(`hello my name is ${name} and my repocount is ${repocount} `)
+
+console.log(name[0])
+
+console.log(name.__proto__)
+
+console.log(name.length)
+
+console.log(name.toUpperCase())
+
+console.log(name.charAt(2))
+
+console.log(name.indexOf('u'))
+
+let newName=name.substring(0,4)
+console.log(newName)
+
+let anotherName=name.slice(2,5)
+console.log(anotherName)
