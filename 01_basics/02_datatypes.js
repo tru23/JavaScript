0b59f949f695f0e385078ceb231defc7cfa86bd3
tru23 <83947699+tru23@users.noreply.github.com>
2024-07-14 19:15:39 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,3 +3,13 @@
 console.log(3+3)
 
 console.log("Hello")
+
+let name="trupti"//string
+let ph=12345//number
+let isLoggedIn=true//boolean
+
+console.log(typeof "trupti")
+console.log(typeof 1234)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
