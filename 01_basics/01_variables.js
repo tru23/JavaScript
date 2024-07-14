ff8b4f56d88d54268f5132bacba00963ab406e3b
tru23 <83947699+tru23@users.noreply.github.com>
2024-07-14 19:10:53 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144522
+let accountEmail="trupti@gmail.com"//prefer this not var
+var accountPassword="12354"
+accountCity="Jaipur"
+
+//accountId=2//not allowed
+
+accountEmail="xyz@gmail.com"
+accountPassword="1212112"
+accountCity="Pune"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
