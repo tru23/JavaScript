e876381e21c0959e93fff99050d99b8ed7bdc252
tru23 <83947699+tru23@users.noreply.github.com>
2024-07-17 11:36:00 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+const myArr=[1,2,3,4,5,6]
+console.log(myArr[1])
+
+const myHeros=["thor","shaktiman","ironman",]
+console.log(myHeros[0])
+
+const myArr2=new Array(1,2,3,4)
+console.log(myArr2[3])
+
+myArr.push(8)
+myArr.push(11)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(9)
+console.log(myArr);
+
+myArr.shift()
+console.log(myArr);
+  
